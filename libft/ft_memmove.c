a2dfea840050cff185407389020e7dd325407a36
Svetlana Titovskaia <stitovsk@c3r4s2.42malaga.com>
2023-10-06 22:14:24 +0200

    reduce memmove

--- libft/ft_memmove.c
+++ libft/ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: stitovsk <stitovsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/14 19:27:12 by stitovsk          #+#    #+#             */
-/*   Updated: 2023/10/06 13:43:47 by stitovsk         ###   ########.fr       */
+/*   Updated: 2023/10/06 17:01:22 by stitovsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,20 +24,18 @@ void	*ft_memmove(void *dst, const void *src, size_t n)
 		return (dst);
 	if (src < dst)
 	{
-		i = n - 1;
-		while (i >= 0)
+		i = n;
+		while (--i >= 0)
 		{
 			*(dest + i) = *(source + i);
-			i--;
 		}
 	}
 	else if (src > dst)
 	{
-		i = 0;
-		while (i < (int)n)
+		i = -1;
+		while (++i < (int)n)
 		{
 			*(dest + i) = *(source + i);
-			i++;
 		}
 	}
 	return (dst);
