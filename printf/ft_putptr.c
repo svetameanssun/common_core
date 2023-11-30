32d3e300bac64221effcf6bfd448e4cfd1789435
Svetlana Titovskaia <stitovsk@c3r7s6.42malaga.com>
2023-11-30 17:44:41 +0100

    ;

--- printf/ft_putptr.c
+++ printf/ft_putptr.c
@@ -6,7 +6,7 @@
 /*   By: stitovsk <stitovsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/24 14:24:55 by stitovsk          #+#    #+#             */
-/*   Updated: 2023/11/30 17:12:39 by stitovsk         ###   ########.fr       */
+/*   Updated: 2023/11/30 17:14:07 by stitovsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void ft_function(void)
 {
 	char *s = "Aunque no le guste a norminette, pongo un string";
-	char *str = "I have created this function to keep ft_putptr":
+	char *str = "I have created this function to keep ft_putptr";
 	return;
 }
 
