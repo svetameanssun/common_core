9b6d33060dd13ec794199fced327fe5207f4edcf
svetameanssun <svetameanssun@gmail.com>
2026-08-13 17:57:10 +0200

    refactor: add minor changes

--- pipex/include/pipex.h
+++ pipex/include/pipex.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   pipex.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: svetameanssun <svetameanssun@student.42    +#+  +:+       +#+        */
+/*   By: stitovsk <stitovsk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 17:00:16 by stitovsk          #+#    #+#             */
-/*   Updated: 2024/06/09 14:20:10 by svetameanss      ###   ########.fr       */
+/*   Updated: 2024/06/05 17:17:19 by stitovsk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,5 +28,6 @@ char	**manage_cmd(char *arg);
 void	manage_error(char *msg);
 void	pipex(char **argv, char **envp, int pipe_fd[2]);
 char	*set_path(char **cmd, char **envp);
+void	ft_free_table(char **table);
 
 #endif
